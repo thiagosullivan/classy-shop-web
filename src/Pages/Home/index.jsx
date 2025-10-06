3e544b6182e28bdd9b177e40d3c32d5f6b070961
thiagosullivan <thiagosullivan12@gmail.com>
2025-10-06 18:39:54 -0300

    feat: add home categories slider

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeSlider from "../../components/HomeSlider";
+import HomeCategoriesSlider from "../../components/CatSlider";
 
 const Home = () => {
   return (
     <>
       <HomeSlider />
+      <HomeCategoriesSlider />
     </>
   );
 };
