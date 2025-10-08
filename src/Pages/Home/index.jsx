b34f098222e9e27babebec73d90779608737811a
thiagosullivan <thiagosullivan12@gmail.com>
2025-10-07 22:16:27 -0300

    feat: free shipping banner

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+import { LiaShippingFastSolid } from "react-icons/lia";
+
 import HomeSlider from "../../components/HomeSlider";
 import HomeCategoriesSlider from "../../components/CatSlider";
 
@@ -7,6 +10,25 @@ const Home = () => {
     <>
       <HomeSlider />
       <HomeCategoriesSlider />
+
+      <section className="py-5 bg-white">
+        <div className="container">
+          <div className="freeShipping w-full p-4 border-1 border-primary rounded-md flex items-center justify-evenly">
+            <div className="col-1 flex items-center gap-4">
+              <LiaShippingFastSolid className="text-5xl" />
+              <span className="text-2xl font-bold">Free Shipping</span>
+            </div>
+            <div className="h-[40px] border-r border-gray-300 w-[1px]" />
+            <div className="col-2">
+              <p className="mb-0 font-medium">
+                Free Delivery Now On Your First Order and over $200
+              </p>
+            </div>
+            <div className="h-[40px] border-r border-gray-300 w-[1px]" />
+            <p className="font-bold text-4xl"> - Only $200</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
