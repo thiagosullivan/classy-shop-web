1af3f46349a41a0672088413c13ffdd495c9f9f6
thiagosullivan <thiagosullivan12@gmail.com>
2025-10-11 10:17:10 -0300

    feat: add description, and tabs to size, infos and comments

--- src/components/QtyBox/index.jsx
+++ src/components/QtyBox/index.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const QtyBox = () => {
-  return <div>QtyBox</div>;
+  const [qtyVal, setQtyVal] = useState(0);
+
+  const plusQty = () => {
+    setQtyVal(qtyVal + 1);
+  };
+
+  const minusQty = () => {
+    if (qtyVal === 0) {
+      setQtyVal(0);
+    } else {
+      setQtyVal(qtyVal - 1);
+    }
+  };
+
+  return (
+    <div className="qtyBox">
+      <div className="relative">
+        <input
+          type="number"
+          className="qty-btn w-full h-[50px] p-2 focus:outline-none border border-[rgba(0,0,0,0.2) rounded-md]"
+          value={qtyVal}
+        />
+        <div className="flex flex-col absolute top-0 right-0">
+          <button
+            className="border-1 border-[rgba(0,0,0,0.2) !h-[25px] !w-[25px]"
+            onClick={() => plusQty()}
+          >
+            +
+          </button>
+          <button
+            className="border-1 border-[rgba(0,0,0,0.2) !h-[25px] !w-[25px]"
+            onClick={() => minusQty()}
+          >
+            -
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QtyBox;
